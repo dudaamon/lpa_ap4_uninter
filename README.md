37099ec907fdedc57c043c282ebc8c48ce5bd1ed
Eduarda Amon <131217736+dudaamon@users.noreply.github.com>
2024-05-16 14:08:37 +0000

    created

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Suponha que você é um colecionador de jogos de videogame. Escreva um algoritmo que permita cadastrar esses jogos informando o nome e a qual videogame ele pertence
-Para isso, crie um menu de opções contendo: cadastrar novo item, listar tudo que foi cadastrado e sair. Para resolver o exercício, crie uma função pra cada item do menu. 
-Além disso, armazene todos os dados em um arquivo de texto que deve ser salvo em disco e manter os dados cadastrados.
+# Aula Prática - Lógica de Programação e Algoritmos
+_Exercício 2 feito em aula_ 
+
+### Suponha que você é um colecionador de jogos de videogame.
+  Escreva um algoritmo que permita cadastrar esses jogos informando o nome e a qual videogame ele pertence. 
+  Para isso, crie um menu de opções contendo: cadastrar novo item, listar tudo que foi cadastrado e sair. Para resolver o exercício, crie uma função pra cada item do menu. 
+  Além disso, armazene todos os dados em um arquivo de texto que deve ser salvo em disco e manter os dados cadastrados.
+
+#python #manipulação_de_arquivos #lógica_de_programação
---
+++ index.py
@@ -0,0 +1,76 @@
+def valida_int(pergunta, min, max):
+  x = int(input(pergunta))
+  while ((x < min) or (x > max)):
+    x = int(input(pergunta))
+  return x
+
+def existeArquivo(nomeArquivo):
+  try:
+    a = open(nomeArquivo, 'rt')
+    a.close()
+  except FileNotFoundError:
+    return False
+  else:
+    return True
+
+def criarArquivo(nomeArquivo):
+  try:
+    a = open(nomeArquivo, 'wt+')
+    a.close()
+  except FileNotFoundError:
+    print('Erro na criação do arquivo')
+  else:
+    print(f'Arquivo {nomeArquivo} criado com sucesso!\n')
+
+def cadastrarJogo(nomeArquivo, nomeJogo, nomeVideogame):
+  if len(nomeJogo) == 0 or len(nomeVideogame) == 0:
+    print('Por favor, preencha todos os campos')
+    return
+
+  try:
+    a = open(nomeArquivo, 'at')
+  except:
+    print('Erro ao abrir o arquivo')
+  else:
+    a.write(f'{nomeJogo} ; {nomeVideogame}\n')
+  finally:
+    a.close()
+
+def listarArquivo(nomeArquivo):
+  try:
+    a = open(nomeArquivo, 'rt')
+  except:
+    print('Erro ao ler o arquivo')
+  else:
+    print(a.read())
+  finally:
+    a.close()
+
+
+arquivo = 'game.txt'
+if existeArquivo(arquivo):
+  print('Arquivo localizado no computador')
+else:
+  print('Arquivo inexistente')
+  criarArquivo(arquivo)
+
+while True:
+  print('MENU')
+  print('1 - Cadastrar novo item')
+  print('2 - Listar cadastros')
+  print('3 - Sair')
+
+  op = valida_int('Escolha a opção desejada: ', 1, 3)
+  if (op == 1): #Novo item
+    print('Opção de cadastrar novo item selecionada...\n')
+    nomeJogo = input('Nome do Jogo: ')
+    nomeVideogame = input('Nome do videogame: ')
+    cadastrarJogo(arquivo, nomeJogo, nomeVideogame)
+  
+  elif (op == 2): #Listar
+    print('Opção de listar selecionada...\n')
+    listarArquivo(arquivo)
+  
+  elif(op == 3):
+    print('Encerrando o programa...')
+    break
